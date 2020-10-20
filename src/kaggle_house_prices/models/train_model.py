f2aea823b2e6544afcd1c148881574500ec2aa52
rcammisola <rocco.cammisola@performgroup.com>
2020-10-20 01:15:23 +0100

    Extract model definition and prediction plot class

--- src/kaggle_house_prices/models/train_model.py
+++ src/kaggle_house_prices/models/train_model.py
@@ -30,17 +30,73 @@ def rmse_cv_score(model, feature_values, target_values, scorer):
                                     scoring=scorer, cv=10))
 
 
+class ModelDefinition:
+    def __init__(self):
+        self.estimator = LinearRegression()
+        self.model = None
+        self.target = "SalePrice"
+        self.input_feature_columns = ["GrLivArea"]
+
+    def target_variable(self):
+        return self.target
+
+    def input_feature_names(self):
+        return self.input_feature_columns
+
+    def fit(self, input_features, y):
+        self.model = self.estimator.fit(input_features, y)
+
+    def predict(self, input_features):
+        return self.model.predict(input_features)
+
+    def score(self, input_features, target_values):
+        scorer = make_scorer(mean_squared_error, greater_is_better=False)
+        return rmse_cv_score(self.model, input_features, target_values, scorer).mean()
+
+    def save(self, model_filename):
+        with open(model_filename, "wb") as model_file_pointer:
+            pickle.dump(self.model, model_file_pointer)
+
+
+class PredictionPlot:
+    def __init__(self,
+                 title="Predicted vs Actual",
+                 training_colour="blue", test_colour="lightgreen"):
+        self.training_colour = training_colour
+        self.test_colour = test_colour
+        self.plot_title = title
+
+    def plot(self, training_predictions, training_actual, test_predictions, test_actual):
+        plt.scatter(training_predictions,
+                    training_actual,
+                    c=self.training_colour,
+                    marker="s",
+                    label="Training data")
+        plt.scatter(test_predictions,
+                    test_actual,
+                    c=self.test_colour,
+                    marker="s",
+                    label="Validation")
+        plt.title(self.plot_title)
+        plt.xlabel("Predicted Values")
+        plt.ylabel("Real Values")
+        plt.legend(loc="upper left")
+        plt.plot([10.5, 13.5], [10.5, 13.5], c="red")
+
+        return plt.gca()
+
+
 if __name__ == "__main__":
     configure_logging()
 
+    model = ModelDefinition()
+
     # Load data
-    train_df = load_training_dataset()
+    train_df = (load_training_dataset()
+                .pipe(log_transform_sale_price))
 
-    # log transform sale price (target)
-    train_df = log_transform_sale_price(train_df)
-
-    y = train_df["SalePrice"]
-    train_df = train_df[["GrLivArea"]]
+    y = train_df[model.target_variable()]
+    train_df = train_df[model.input_feature_names()]
 
     # train/test split
     X_train, X_test, y_train, y_test = train_test_split(
@@ -50,17 +106,18 @@ if __name__ == "__main__":
         random_state=0
     )
 
-    with mlflow.start_run() as run:
-        mlflow.log_params({"Features": "GrLivArea"})
+    with mlflow.start_run(run_name="baseline") as run:
+        mlflow.log_params({
+            "Features": model.input_feature_names(),
+            "Training set size": X_train.shape[0],
+            "Test set size": X_test.shape[0]
+        })
 
-        # training fit
-        model = LinearRegression()
         model.fit(X_train, y_train)
 
         # assess vs training and test set
-        scorer = make_scorer(mean_squared_error, greater_is_better=False)
-        training_rmse = rmse_cv_score(model, X_train, y_train, scorer).mean()
-        test_rmse = rmse_cv_score(model, X_test, y_test, scorer).mean()
+        training_rmse = model.score(X_train, y_train)
+        test_rmse = model.score(X_test, y_test)
 
         logging.info(f"RMSE on Training set: {training_rmse}")
         logging.info(f"RMSE on Test set: {test_rmse}")
@@ -96,21 +153,8 @@ if __name__ == "__main__":
         plt.show()
 
         # Plot predictions
-        plt.scatter(y_training_predictions,
-                    y_train,
-                    c="blue",
-                    marker="s",
-                    label="Training data")
-        plt.scatter(y_test_predictions,
-                    y_test,
-                    c="lightgreen",
-                    marker="s",
-                    label="Validation")
-        plt.title("Linear Regression")
-        plt.xlabel("Predicted Values")
-        plt.ylabel("Real Values")
-        plt.legend(loc="upper left")
-        plt.plot([10.5, 13.5], [10.5, 13.5], c="red")
+        predicted_plot = PredictionPlot(title="Linear Regression")
+        ax = predicted_plot.plot(y_training_predictions, y_train, y_test_predictions, y_test)
 
         predictions_path = "./reports/figures/baseline/baseline_linear_regression_predictions.png"
         plt.savefig(predictions_path)
