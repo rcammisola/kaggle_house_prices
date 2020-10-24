e4fbaa8e61e535fff112ba909720f0058e8750fc
rcammisola <rocco.cammisola@performgroup.com>
2020-10-24 01:24:37 +0100

    Move plotting code away from training process

--- src/kaggle_house_prices/train.py
+++ src/kaggle_house_prices/train.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from kaggle_house_prices.data.make_dataset import load_training_dataset
 from kaggle_house_prices.logs import configure_logging
 from kaggle_house_prices.models.baseline_model import ModelDefinition
+from kaggle_house_prices.visualization.diagnostic import PredictionPlot, plot_residuals
 
 
 def log_transform_sale_price(df):
@@ -14,60 +15,8 @@ def log_transform_sale_price(df):
     return df
 
 
-class PredictionPlot:
-    def __init__(self,
-                 title="Predicted vs Actual",
-                 training_colour="blue", test_colour="lightgreen"):
-        self.training_colour = training_colour
-        self.test_colour = test_colour
-        self.plot_title = title
-
-        self.fig, self.ax = plt.subplots(1, 1)
-
-    def plot(self, training_predictions, test_predictions, training_actual, test_actual):
-        self.ax.scatter(training_predictions,
-                        training_actual,
-                        c=self.training_colour,
-                        marker="s",
-                        label="Training data")
-        self.ax.scatter(test_predictions,
-                        test_actual,
-                        c=self.test_colour,
-                        marker="s",
-                        label="Validation")
-        self.ax.set_title(self.plot_title)
-        self.ax.set_xlabel("Predicted Values")
-        self.ax.set_ylabel("Real Values")
-        self.ax.legend(loc="upper left")
-        self.ax.plot([10.5, 13.5], [10.5, 13.5], c="red")
-
-        return self.ax
-
-    def save(self, file_path):
-        self.fig.savefig(file_path)
-
-
-def plot_residuals(training_predictions, test_predictions, training_actual, test_actual):
-    plt.scatter(training_predictions,
-                training_predictions - training_actual,
-                c="blue",
-                marker="s",
-                label="Training data")
-    plt.scatter(test_predictions,
-                test_predictions - test_actual,
-                c="lightgreen",
-                marker="s",
-                label="Validation data")
-    plt.title("Linear Regression")
-    plt.xlabel("Predicted Values")
-    plt.ylabel("Residuals")
-    plt.legend(loc="upper left")
-    plt.hlines(y=0, xmin=10.5, xmax=13.5, color="red")
-
-
-def preprocessing_pipeline(model_definition):
-    train_df = (load_training_dataset()
-                .pipe(log_transform_sale_price))
+def preprocessing_pipeline(dataset, model_definition):
+    train_df = dataset.pipe(log_transform_sale_price)
 
     y = train_df[model_definition.target_variable()]
     train_df = train_df[model_definition.input_feature_names()]
@@ -117,6 +66,6 @@ if __name__ == "__main__":
 
     model = ModelDefinition()
 
-    X_train, X_test, y_train, y_test = preprocessing_pipeline(model)
+    X_train, X_test, y_train, y_test = preprocessing_pipeline(load_training_dataset(), model)
 
     train_model(model, X_train, X_test, y_train, y_test)
---
+++ src/kaggle_house_prices/visualization/diagnostic.py
@@ -0,0 +1,52 @@
+from matplotlib import pyplot as plt
+
+
+class PredictionPlot:
+    def __init__(self,
+                 title="Predicted vs Actual",
+                 training_colour="blue", test_colour="lightgreen"):
+        self.training_colour = training_colour
+        self.test_colour = test_colour
+        self.plot_title = title
+
+        self.fig, self.ax = plt.subplots(1, 1)
+
+    def plot(self, training_predictions, test_predictions, training_actual, test_actual):
+        self.ax.scatter(training_predictions,
+                        training_actual,
+                        c=self.training_colour,
+                        marker="s",
+                        label="Training data")
+        self.ax.scatter(test_predictions,
+                        test_actual,
+                        c=self.test_colour,
+                        marker="s",
+                        label="Validation")
+        self.ax.set_title(self.plot_title)
+        self.ax.set_xlabel("Predicted Values")
+        self.ax.set_ylabel("Real Values")
+        self.ax.legend(loc="upper left")
+        self.ax.plot([10.5, 13.5], [10.5, 13.5], c="red")
+
+        return self.ax
+
+    def save(self, file_path):
+        self.fig.savefig(file_path)
+
+
+def plot_residuals(training_predictions, test_predictions, training_actual, test_actual):
+    plt.scatter(training_predictions,
+                training_predictions - training_actual,
+                c="blue",
+                marker="s",
+                label="Training data")
+    plt.scatter(test_predictions,
+                test_predictions - test_actual,
+                c="lightgreen",
+                marker="s",
+                label="Validation data")
+    plt.title("Linear Regression")
+    plt.xlabel("Predicted Values")
+    plt.ylabel("Residuals")
+    plt.legend(loc="upper left")
+    plt.hlines(y=0, xmin=10.5, xmax=13.5, color="red")
